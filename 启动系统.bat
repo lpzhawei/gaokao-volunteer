1cf60253bdc112176d8ccd59b8e01188b3dd2cee
lpzhawei <lpzhawei@users.noreply.github.com>
2026-04-12 02:01:08 +0800

    Changes to be committed:     modified:   "启动系统.bat"

--- 启动系统.bat
+++ 启动系统.bat
@@ -2,7 +2,7 @@
 chcp 65001 > nul
 cd /d "%~dp0"
 echo 正在启动河北省高考志愿填报系统...
-venv\Scripts\python.exe main.py
+..\venv\Scripts\python.exe main.py
 if %errorlevel% neq 0 (
     echo 启动失败，请检查环境配置
     pause
